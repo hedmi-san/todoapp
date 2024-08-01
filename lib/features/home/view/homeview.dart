267cd3dc9927215fb5b5d1d6a56c1b864a8d7f00
hedmi-san <88062690+hedmi-san@users.noreply.github.com>
2024-08-01 16:40:17 +0100

    Updating the UI

--- lib/features/home/view/homeview.dart
+++ lib/features/home/view/homeview.dart
@@ -1,4 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todoapp/todo_bloc/todo_bloc.dart';
+
+import '../../../data/todo.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,8 +13,55 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  addTodo(Todo todo) {
+    context.read<TodoBloc>().add(AddTodo(todo));
+  }
+
+  removeTodo(Todo todo) {
+    context.read<TodoBloc>().add(RemoveTodo(todo));
+  }
+
+  alterTodo(int index) {
+    context.read<TodoBloc>().add(AlterTodo(index));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        child: const Icon(
+          color: Colors.black,
+          CupertinoIcons.add,
+        ),
+      ),
+      appBar: AppBar(
+        title: const Text(
+          'My Todo App',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8),
+        child: BlocBuilder<TodoBloc, TodoState>(
+          builder: (context, state) {
+            if (state.status == TodoStatus.success) {
+              return Container();
+            } else if (state.status == TodoStatus.intial) {
+              return const Center(child: CircularProgressIndicator());
+            } else {
+              return Container();
+            }
+          },
+        ),
+      ),
+    );
   }
 }
